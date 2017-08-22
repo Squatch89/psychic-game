9966b93b04d12a1cc5314d52a81ee3d865fca31c
Gene Williams <GeneWillb@Gmail.com>
2017-08-22 18:48:09 -0400

    working psychic game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,8 +4,6 @@ var guessesLeft = 10;
 var lettersGuessed = [];
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-
-
 //chooses a letter to be guessed
     var selectedLetter = letters[Math.floor((Math.random() * (letters.length - 1)))];
     console.log(selectedLetter + " this is the selected letter");
@@ -30,42 +28,12 @@ var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
         console.log(selectedLetter + " this is the selected letter");
     }
 
-
-        var alpha = {
-            65: "a",
-            66: "b",
-            67: "c",
-            68: "d",
-            69: "e",
-            70: "f",
-            71: "g",
-            72: "h",
-            73: "i",
-            74: "j",
-            75: "k",
-            76: "l",
-            77: "m",
-            78: "n",
-            79: "o",
-            80: "p",
-            81: "q",
-            82: "r",
-            83: "s",
-            84: "t",
-            85: "u",
-            86: "v",
-            87: "w",
-            88: "x",
-            89: "y",
-            90: "z",
-        };
-    
 //displays the letters guessed
     document.onkeyup = function (event) {
         var userInput = event.key;
-       
         
-        if ((lettersGuessed.indexOf(userInput) < 0)) {
+        
+        if ( (lettersGuessed.indexOf(userInput) < 0) && (letters.indexOf(userInput) >= 0) ) {
             
             
             // console.log(typeof userInput + " this was the user input");
@@ -75,25 +43,24 @@ var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
             var lettersChosenNode = document.createTextNode(" " + lettersGuessed[lettersGuessed.length - 1]);
             document.getElementById("letters").appendChild(lettersChosenNode);
             
+                //checks for win and loss conditions and updates the values displayed
+                if (String(lettersGuessed[lettersGuessed.length - 1]) === String(selectedLetter)) {
+                    wins++;
+                    console.log(wins + " this was a win");
+                    document.getElementById("wins").textContent = wins;
+                    resetGame();
+                }
+                else
+                {
+                    guessesLeft--;
+                    console.log(guessesLeft + " this was a guess");
+                    document.getElementById("guessesLeft").textContent = guessesLeft;
+                }
+            
         }
         
         console.log(lettersGuessed);
-    
-//checks for win and loss conditions and updates the values displayed
-        if (String(lettersGuessed[lettersGuessed.length - 1]) === String(selectedLetter)) {
-            wins++;
-            console.log(wins + " this was a win");
-            document.getElementById("wins").textContent = wins;
-            resetGame();
-            
-        }
-        else
-        {
-            guessesLeft--;
-            console.log(guessesLeft + " this was a guess");
-            document.getElementById("guessesLeft").textContent = guessesLeft;
-        }
-    
+        //determines if you have lost and updates score if so
         if (guessesLeft === 0)
         {
             losses++;
@@ -102,6 +69,6 @@ var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
             resetGame();
         }
         
-        };
+    };
 
  
